7bdf2b141aaa0db439124d7e15e7a157c9d1cf48
sp4m-08 <roysaunok@gmail.com>
2025-06-20 14:18:28 +0530

    Fully readable and organised files structure for implementing pages

---
+++ src/app/Page2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Content from "../pages/Content";
+
+const Page2 = () => {
+  return (
+    //for future additional page
+
+    <div>
+      <Content />
+    </div>
+  );
+};
+
+export default Page2;
